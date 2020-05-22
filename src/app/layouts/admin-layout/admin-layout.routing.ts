1da86397644cd6d4657a0ef27f8b01c18a390d1a
ghost4967 <4967john4967@gmail.com>
2020-05-22 16:36:22 -0400

    template

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-
-export const AdminLayoutRoutes: Routes = [
-    { path: 'user-profile',   component: UserProfileComponent },
-];
