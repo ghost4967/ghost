f43939b8500b0947bb5f626ac6b4c7e4fc770aed
ghost4967 <4967john4967@gmail.com>
2020-05-28 16:44:13 -0400

    change template

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { ProductComponent } from '../../components/product/product.component';
-
-export const AdminLayoutRoutes: Routes = [
-    { path: 'product',   component: ProductComponent },
-];
