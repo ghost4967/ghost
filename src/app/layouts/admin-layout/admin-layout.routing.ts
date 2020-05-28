9db536226bf3d1b0a8020c09ea10d4d27231d453
ghost4967 <4967john4967@gmail.com>
2020-05-28 16:51:00 -0400

    new template

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { ProductComponent } from '../../pages/product/product.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'product',      component: ProductComponent },
+];
---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    _id?: string,
+    name: String,
+    price: number,
+    quantity: number;
+    typeUnity: String;
+    description: String,
+    image: String,
+}
